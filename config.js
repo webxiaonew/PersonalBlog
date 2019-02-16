4e7638774fa9c8d9fbd99ca036bf5a01714cf5ea
yangbashan <15733097205@163.com>
2019-02-16 18:23:19 +0800

    init

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 var globalConfig = {};
 var conf =  fs.readFileSync("./server.conf");
-var configArr = conf.toString().split("\r\n");
+var configArr = conf.toString().split("\n");
 for (var i = 0;i < configArr.length; i++){
     globalConfig[configArr[i].split("=")[0].trim()] = configArr[i].split("=")[1].trim();
 }
