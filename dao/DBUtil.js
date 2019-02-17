9ab66026a88189d91c8c7eb3aadf5603b1981eea
yangbashan <15733097205@163.com>
2019-02-17 14:47:58 +0800

    init

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql")
 
 function createConnection(){
     var connection  = mysql.createConnection({
-        host:"192.168.1.7",
+        host:"192.168.1.8",
         port:"3306",
         user:"root",
         password:"15733097205.",
